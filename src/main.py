73802d601a5ee3bef77513612236dd6098fd46ff
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2021-09-08 17:18:46 +0900

    Security- First Steps

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
 
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="token")
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root(token: str = Depends(oauth2_schema)):  # noqa: B008
+    return {"token": token}
