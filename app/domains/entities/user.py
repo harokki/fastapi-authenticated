6c4b66886aff39a1fdb696b2c4faa65f3f385b55
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2021-09-09 18:05:17 +0900

    Userにaccount_nameを追加

--- app/domains/entities/user.py
+++ app/domains/entities/user.py
@@ -8,5 +8,6 @@ class User(Base):
 
     username = Column(String, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
+    account_name = Column(String, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
