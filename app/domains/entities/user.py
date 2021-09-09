169b6d2ee005feb310f0d247ffac2cad7c53ed05
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2021-09-09 18:26:43 +0900

    Userにカラムを追加

--- app/domains/entities/user.py
+++ app/domains/entities/user.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Boolean, Column, String
+from datetime import datetime, timezone
+
+from sqlalchemy import TIMESTAMP, Boolean, Column, String
 
 from app.database import Base
 
@@ -6,8 +8,30 @@ from app.database import Base
 class User(Base):
     __tablename__ = "users"
 
-    username = Column(String, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    account_name = Column(String, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    username = Column(String(32), primary_key=True, index=True)
+    email = Column(String(256), unique=True, index=True, nullable=False)
+    account_name = Column(String(32), index=True, nullable=False)
+    hashed_password = Column(String(64), nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), default=datetime.now, nullable=False)
+    created_by = Column(String(32), nullable=False)
+    updated_at = Column(TIMESTAMP(timezone=True), default=datetime.now, nullable=False)
+    updated_by = Column(String(32), nullable=False)
+
+    def __init__(
+        self,
+        username: str,
+        email: str,
+        account_name: str,
+        hashed_password: str,
+        created_by: str,
+    ):
+        self.username = username
+        self.email = email
+        self.account_name = account_name
+        self.hashed_password = hashed_password
+        self.is_active = True
+        self.created_at = datetime.now(timezone.utc)
+        self.created_by = created_by
+        self.updated_at = self.created_at
+        self.updated_by = created_by
--- tests/domains/entities/test_user.py
+++ tests/domains/entities/test_user.py
@@ -5,11 +5,17 @@ def test_initialize_user():
     user = User(
         username="john",
         email="john@example.com",
+        account_name="ジョン",
         hashed_password="aaaaa",
-        is_active=True,
+        created_by="john",
     )
 
     assert user.username == "john"
     assert user.email == "john@example.com"
+    assert user.account_name == "ジョン"
     assert user.hashed_password == "aaaaa"
     assert user.is_active is True
+    assert user.created_by == "john"
+    assert user.created_at
+    assert user.updated_by == "john"
+    assert user.updated_at
